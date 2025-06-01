0ea4ed78f96bad985668a0a20d1424b1bea15fee
ymizin <ymizin@team.amocrm.com>
2025-06-01 23:33:42 +0300

    рефакт

--- src/widgets/TodoList/index.test.tsx
+++ src/widgets/TodoList/index.test.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 
 import React from 'react';
 
-import { render, fireEvent, queryAllByText } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { List } from './ui/List/List';
 
